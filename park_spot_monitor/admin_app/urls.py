53e94e67cccaab2f2422324f69cc2f0fb41f3943
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2024-04-08 00:05:12 +0200

    update code

---
+++ park_spot_monitor/admin_app/urls.py
@@ -0,0 +1,13 @@
+# urls.py
+
+from django.urls import path
+from . import views
+
+app_name = 'admin_app'
+
+urlpatterns = [
+    path('add_number_plate/', views.add_number_plate, name='add_number_plate'),
+    path('delete_number_plate/<int:plate_id>/', views.delete_number_plate, name='delete_number_plate'),
+    path('configure_parking_rates/', views.configure_parking_rates, name='configure_parking_rates'),
+    path('add_to_blacklist/', views.add_to_blacklist, name='add_to_blacklist'),
+]

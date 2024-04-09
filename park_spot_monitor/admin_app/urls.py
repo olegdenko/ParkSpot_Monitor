55e1caa1860fce8c3433dd6b7ba6e6209cf6353c
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2024-04-09 02:03:58 +0200

    Added styles to main page

--- park_spot_monitor/admin_app/urls.py
+++ park_spot_monitor/admin_app/urls.py
@@ -1,13 +1,13 @@
-# urls.py
+# # urls.py
 
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-app_name = 'admin_app'
+# app_name = 'admin_app'
 
-urlpatterns = [
-    path('add_number_plate/', views.add_number_plate, name='add_number_plate'),
-    path('delete_number_plate/<int:plate_id>/', views.delete_number_plate, name='delete_number_plate'),
-    path('configure_parking_rates/', views.configure_parking_rates, name='configure_parking_rates'),
-    path('add_to_blacklist/', views.add_to_blacklist, name='add_to_blacklist'),
-]
+# urlpatterns = [
+#     path('add_number_plate/', views.add_number_plate, name='add_number_plate'),
+#     path('delete_number_plate/<int:plate_id>/', views.delete_number_plate, name='delete_number_plate'),
+#     path('configure_parking_rates/', views.configure_parking_rates, name='configure_parking_rates'),
+#     path('add_to_blacklist/', views.add_to_blacklist, name='add_to_blacklist'),
+# ]

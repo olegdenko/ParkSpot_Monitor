9e4dcea457e6ceffb36184b1ff3a3d25d111800d
Chumak Nikita <117525795+Nikita-devel@users.noreply.github.com>
2024-04-10 08:35:33 +0200

    Додав middleware для перевірки балансу

--- park_spot_monitor/park_spot_monitor/settings.py
+++ park_spot_monitor/park_spot_monitor/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main_app.middleware.BalanceCheckMiddleware', # Додав middleware для перевірки балансу
 ]
 
 ROOT_URLCONF = 'park_spot_monitor.urls'

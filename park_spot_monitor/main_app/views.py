728e938c374e9b0c402d725ae2b5d5d12e88a9e7
Chumak Nikita <117525795+Nikita-devel@users.noreply.github.com>
2024-04-10 08:41:14 +0200

    Update views.py

--- park_spot_monitor/main_app/views.py
+++ park_spot_monitor/main_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'main_app/index.html', {"title": "Main page"})
+
+def balance_insufficient(request):
+    return render(request, 'main_app/balance_insufficient.html')

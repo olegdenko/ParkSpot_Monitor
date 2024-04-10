e28ff9636404848280e0434c25af7ac9847f671a
Cheburator <sashadanilenko06@gmail.com>
2024-04-11 00:49:01 +0200

    displaying user's plates on main page added

--- park_spot_monitor/main_app/views.py
+++ park_spot_monitor/main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from users.models import Plates
+
 # Create your views here.
 def main(request):
-    return render(request, 'main_app/index.html', {"title": "Main page"})
+    plates = None
+    if request.user.is_authenticated:
+        plates = Plates.objects.all()
+    
+    return render(request, 'main_app/index.html', {"title": "Main page", "plates": plates})

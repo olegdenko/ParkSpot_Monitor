5e703923f2e6ed149fa14ff231fc8190d7cd5b7c
Nilinz <greyhort@gmail.com>
2024-04-15 19:58:12 +0300

    problem Error processing plate: null value in column user_id of relation users_plates violates not-null constraint

--- park_spot_monitor/number_recognition/views.py
+++ park_spot_monitor/number_recognition/views.py
@@ -3,27 +3,25 @@ from django.utils.decorators import method_decorator
 from django.views.generic import View
 from users.models import Plates, Sessions
 from .number_recognition import recognize_plate
-from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 import cv2
-from admin_app.forms import UploadImageForm
+from admin_app.forms import UploadImageForm 
 from django.shortcuts import render
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UploadImageView(View):
     def get(self, request, *args, **kwargs):
         initial_data = {
-            'image': None,  
+            'image': None,
         }
 
-        form = UploadImageForm(initial=initial_data)  
+        form = UploadImageForm(initial=initial_data)
 
         context = {
-            'form': form,  
+            'form': form,
         }
 
-        return render(request, 'number_recognition/number_recognition.html', context)    
-
+        return render(request, 'number_recognition/number_recognition.html', context)
 
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
@@ -32,26 +30,17 @@ class UploadImageView(View):
 
             if plate_number:
                 try:
-                    
-                    plate = Plates.objects.get(plate=plate_number)
-                    
-                    last_session = Sessions.objects.filter(plate=plate).order_by('-entrance_time').first()
-                    
-                    if last_session and not last_session.exit_time:
-                        
-                        last_session.exit_time = timezone.now()
-                        last_session.save()
-                        return HttpResponse(f"Машина з номером {plate_number} виїхав.")
-                    else:
-                        
-                        session = Sessions.objects.create(plate=plate)
-                        return HttpResponse(f"Машина з номером {plate_number} заїхав.")
-                except Plates.DoesNotExist:
-                    plate = Plates.objects.create(plate=plate_number)
-                    session = Sessions.objects.create(plate=plate)
 
-                    return HttpResponse(f"Машина з номером {plate_number} заїхав.")
+                    plate = Plates.objects.create(plate=plate_number)
+
+                    if request.user.is_authenticated:
+                        user = request.user
+                        session = Sessions.objects.create(plate=plate, user=user)
+                        return HttpResponse(f"Номер розпізнано: {plate_number}. Сесія створена.")
+                    else:
+                        return HttpResponse("Для створення сесії, потрібно увійти до системи.")
+                except Exception as e:
+                    print(f"Error processing plate: {e}")
+                    return HttpResponse("Помилка обробки даних. Спробуйте ще раз.")
             else:
                 return HttpResponse("Номер не розпізнано")
-
-

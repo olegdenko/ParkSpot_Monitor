025b0db34c6876e36ceff9d202689be24ca7c1f9
Cheburator <sashadanilenko06@gmail.com>
2024-04-17 20:57:32 +0200

    bugs fixed: balance, add plate redirect, all plates displaying

--- park_spot_monitor/main_app/views.py
+++ park_spot_monitor/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from users.models import Balance
 
 
 def main(request):
+    Balance.objects.get_or_create(user=request.user)
     return render(request, 'main_app/index.html', {"title": "Main page"})

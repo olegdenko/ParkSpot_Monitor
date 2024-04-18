f744a4f98d10fc220c565d4c95e14bcffae21dfb
Denko <officedenko@gmail.com>
2024-04-18 14:47:22 +0300

    Docker

--- park_spot_monitor/docker/dev/entrypoint.sh
+++ park_spot_monitor/docker/dev/entrypoint.sh
@@ -5,5 +5,5 @@ echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 echo "Waiting for PostgreSQL to start..."
 ./wait-for-it.sh postgres:5432 --timeout=30
 echo "PostgreSQL has started"
-poetry run python manage.py migrate 
+poetry run python manage.py migrate
 exec "$@"
